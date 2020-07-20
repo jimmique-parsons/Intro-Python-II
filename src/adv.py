7dc1018a81da9eaeb742373073bcf2a33f9f8fb0
Jimmique <jimmique88@gmail.com>
2020-07-19 23:25:24 -0700

    Adjusted print message formatting, added on_take and on_drop methods, and added handler code for drop <item

--- src/adv.py
+++ src/adv.py
@@ -41,6 +41,17 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
+
+def get_item(item_list, name):
+    for item in item_list:
+        try:
+            item_name = getattr(item, 'name')
+            if item_name == name:
+                return item
+        except AttributeError:
+            return None
+
+
 # Make a new player object that is currently in the 'outside' room.
 player = Player('Jimmique', room['outside'])
 
@@ -53,20 +64,52 @@ while selection != 'q':
 # * Prints the current room name
 print(current_room)
 # * Waits for user input and decides what to do.
-selection = input('\nSelect a cardinal direction: n, s, w, e, or q to quit \n')
-# If the user enters a cardinal direction, attempt to move to the room there.
-if selection in ['n', 's', 'w', 'e', 'q']:
+selection = input('''
+     ****
+     Select:\n
+     A cardinal direction: 'n', 's', 'w', 'e' \n
+     An item to get: 'get' or 'take' <item name> \n
+     An item to drop: 'drop' <item name> \n
+     or 'q' to quit \n
+     ****
+     ''')
 
-    direction = f'{selection}_to'
+selection_args = selection.split(' ')
+command = selection_args[0]
+item = None
+
+if len(selection_args) > 1:
+    item = selection_args[1]
+
+# If the user enters "q", quit the game.
+if command == 'q':
+    print("Thanks for playing!")
+# If the user enters a cardinal direction, attempt to move to the room there.
+if selection in ['n', 's', 'w', 'e']:
+
+    direction = f'{command}_to'
     try:
         player.current_room = getattr(current_room, direction)
         print('\n')
 
 # Print an error message if the movement isn't allowed.
-    except AttributeError as a:
-        print('\nNot able to move in that direction!\n')
-else:
-    print('\nPlease enter a valid selection\n')
+    except AttributeError:
+        print('\n*****\nNot able to move in that direction!\n*****\n')
+elif command in ['get', 'take'] and item is not None:
+    wanted_item = get_item(current_room.items, item)
 
-# If the user enters "q", quit the game.
-print("Thanks for playing!")
+    if wanted_item is not None:
+        current_room.items.remove(wanted_item)
+        player.items.append(wanted_item)
+        print(wanted_item.on_take())
+    else:
+        print('\n*****\n Item not available!\n*****\n')
+elif command == 'drop' and item is not None:
+    item_to_drop = get_item(player.items, item)
+
+    if item_to_drop is not None:
+        player.items.remove(item_to_drop)
+        current_room.items.append(item_to_drop)
+        print(item_to_drop.on_drop())
+else:
+    print('\n*****\nInvalid selection!\n*****\n')
--- src/item.py
+++ src/item.py
@@ -5,3 +5,9 @@ class Item:
 
     def __str__(self):
         return f'Name: {self.name}, Description: {self.description}'
+
+    def on_take(self):
+        return f'\n You have picked up {self.name} \n'
+
+    def on_drop(self):
+        return f'\n You have dropped {self.name} \n'
